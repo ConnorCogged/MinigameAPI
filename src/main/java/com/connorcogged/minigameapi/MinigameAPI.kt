23aae65cc6b5e84748717e3de8064c7c0d08cdd7
Your Name <connorejohnson282@gmail.com>
2023-11-23 12:16:22 +0700

    inital commit

---
+++ src/main/java/com/connorcogged/minigameapi/MinigameAPI.kt
@@ -0,0 +1,13 @@
+package com.connorcogged.minigameapi
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MinigameAPI : JavaPlugin() {
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+}
